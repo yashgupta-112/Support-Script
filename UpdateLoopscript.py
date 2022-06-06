25fb0eb8265c4715ce86e93fa07e0c0d89c1255e
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-06 19:32:22 +0530

    final-test1

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -1,8 +1,9 @@
 from subprocess import check_output
 
-status =  check_output("app-plex upgrade",   shell=True)
-status = status.decode("utf-8")
-if "true" in status:
-    print("success")
-elif "false" in status:
-    print("failure")
+while True:
+    status =  check_output("app-plex upgrade",   shell=True)
+    status = status.decode("utf-8")
+    if "true" in status:
+        break
+    elif "false" in status:
+        status =  check_output("app-plex upgrade",   shell=True)
