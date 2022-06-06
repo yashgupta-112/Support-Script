e172417294f24f2651ce2db7bad6e4b8983a5c74
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-06 19:50:22 +0530

    final-use

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -2,7 +2,7 @@ from subprocess import check_output
 appname = input("Please application name you want to upgrade: ")
 
 while True:
-    status =  check_output("app-{app} upgrade",   shell=True).format(appname)
+    status =  check_output(f"app-{appname} upgrade",   shell=True)
     status = status.decode("utf-8")
     if "true" in status:
         print("succes")
