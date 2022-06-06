838122763386cabeb25c4c1799f27878dad94284
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-06 19:33:56 +0530

    final-test2

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -4,6 +4,8 @@ while True:
     status =  check_output("app-plex upgrade",   shell=True)
     status = status.decode("utf-8")
     if "true" in status:
+        print("succes")
         break
     elif "false" in status:
         status =  check_output("app-plex upgrade",   shell=True)
+        print("fail")
