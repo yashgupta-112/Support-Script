c06fa5d3215419cdc97c8d78ef2a7c26471c92c5
Yash Gupta <85554972+yashgupta-112@users.noreply.github.com>
2022-06-07 11:28:21 +0530

    Update UpdateLoopscript.py

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -5,7 +5,7 @@ while True:
     status =  check_output(f"app-{appname} upgrade",   shell=True)
     status = status.decode("utf-8")
     if "true" in status:
-        print("succes")
+        print("success")
         break
     elif "false" in status:
         status =  check_output("app-plex upgrade",   shell=True)
