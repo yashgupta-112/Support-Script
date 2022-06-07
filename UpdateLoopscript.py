20eda6cfcfef0b2b17f89d78c4118112ce98a941
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2022-06-07 15:36:15 +0530

    Update UpdateLoopscript.py

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -1,13 +1,32 @@
-from subprocess import check_output
-appname = input("Please application name you want to upgrade: ")
+import os
+import sys
+from subprocess import check_output, check_call
 
-while True:
-    status =  check_output(f"app-{appname} upgrade",   shell=True)
-    status = status.decode("utf-8")
-    if "true" in status:
-        print("success")
-        break
-    elif "false" in status:
-        status =  check_output(f"app-{appname} upgrade",   shell=True)
-        print("fail")
+#install halo package if it's not there
+def package_install(package):
+    FNULL = open(os.devnull, 'w')
+    check_call([sys.executable, "-m", "pip", "install", package], stdout=FNULL)
 
+#check halo package for spinner
+try:
+    import halo
+except ImportError as e:
+    package_install('halo')
+    import halo
+#App Upgrade
+finally:
+    spinner = halo.Halo(text='In-Progress', text_color='magenta', spinner='bouncingBar')
+    appname = input("Enter AppName to upgrade: ")
+
+    spinner.start()
+    count = 1
+    while True:
+        status = check_output(f"app-{appname} upgrade".format(appname=appname), shell=True)
+        status = status.decode("utf-8")
+        if "true" in status:
+            spinner.stop()
+            print("[SUCCESS] Upgrade Count:", count)
+            break
+        elif "false" in status:
+            count +=1
+            
