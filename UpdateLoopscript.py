08ddc584f5acd7f1e9da259ec7b55a397fa6c1c0
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-08 17:58:04 +0530

    import fix commit

--- UpdateLoopscript.py
+++ UpdateLoopscript.py
@@ -2,29 +2,22 @@ import os
 import sys
 from subprocess import check_output, check_call
 
+check_output("pip install halo",shell=True)
 
 
-try:
-    import halo
-except ImportError:
-    FNULL = open(os.devnull, 'w')
-    check_call([sys.executable, "-m", "pip", "install", 'halo'], stdout=FNULL)
+import halo
+spinner = halo.Halo(text='In-Progress', text_color='magenta', spinner='bouncingBar')
+appname = input("Enter AppName to upgrade: ")
 
-#App Upgrade
-finally:
-    import halo
-    spinner = halo.Halo(text='In-Progress', text_color='magenta', spinner='bouncingBar')
-    appname = input("Enter AppName to upgrade: ")
-
-    spinner.start()
-    count = 1
-    while True:
-        status = check_output(f"app-{appname} upgrade".format(appname=appname), shell=True)
-        status = status.decode("utf-8")
-        if "true" in status:
-            spinner.stop()
-            print("[SUCCESS] Upgrade Count:", count)
-            break
-        elif "false" in status:
-            count +=1
-            
+spinner.start()
+count = 1
+while True:
+    status = check_output(f"app-{appname} upgrade".format(appname=appname), shell=True)
+    status = status.decode("utf-8")
+    if "true" in status:
+        spinner.stop()
+        print("[SUCCESS] Upgrade Count:", count)
+        break
+    elif "false" in status:
+        count +=1
+        
