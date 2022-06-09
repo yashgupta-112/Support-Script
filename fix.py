39ad8cde9fc0fdc087aeaddd53e3d9d2569bb382
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-09 19:31:19 +0530

    yo commit

--- fix.py
+++ fix.py
@@ -1,23 +1,18 @@
 import os
 import sys
+import subprocess
 from subprocess import check_output, check_call
-from pip._internal import main 
-import importlib
+
 #install halo package if it's not there
-os.system("python3 -m pip install --upgrade pip")
-#check halo package for spinner
-try:
-    importlib.import_module('halo')
-except ImportError:
-    main(['install', 'halo'])
-    os.system("clear")
-    
-#App Upgrade
-finally:
+def package_install(package):
+    subprocess.call(["/usr/bin/pip3", "install", "--user", "-q", "--no-cache-dir", "--disable-pip-version-check", package])
+    os.execl(sys.executable, sys.executable, *sys.argv) #re-compile code
+
+# App upgrade function
+def app_upgrade():
     import halo
     spinner = halo.Halo(text='In-Progress', text_color='magenta', spinner='bouncingBar')
     appname = input("Enter AppName to upgrade: ")
-
     spinner.start()
     count = 1
     while True:
@@ -29,3 +24,21 @@ finally:
             break
         elif "false" in status:
             count +=1
+
+
+def importhalo_runcode():
+    try:
+        check_halo = os.popen("pip3 list | grep halo").read()
+        if not check_halo:
+            package_install('halo')
+    except Exception as e:
+        print("Unable to install halo module. Try manually 'pip3 install halo'")
+    finally:
+        app_upgrade()
+
+
+def main():
+    importhalo_runcode()
+
+if __name__ == '__main__':
+    main()
